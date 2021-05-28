cba08ff0dafb4fd1f75f574f89983baf6e7f0e1b
TatianaMihailov <76222261+TatianaMihailov@users.noreply.github.com>
2021-05-28 19:51:35 +0300

    Add files via upload

--- javascript/fisierScript.js
+++ javascript/fisierScript.js
@@ -1,4 +1,7 @@
-<script>
-alert("Incepem studierea JavaScript!");
-</script>
+function myFunction(){
+alert(location.hostname);
+//alert("Incepem studierea JavaScript!");
+prompt('cati ani ai?');
+}
+
  
